d5cc16c70e5c3aa098fd50348c44e25e301779fc
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-03-11 22:19:17 +0100

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/app/netlify/stamenkovskikevin/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/netlify/stamenkovskikevin/ExampleInstrumentedTest.java
@@ -1,5 +1,4 @@
 package app.netlify.stamenkovskikevin;
-
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
